35f9854efa294c6836aaa03d57e6fc53a9e470a9
ameni <bouattour.ameni@gmail.com>
2016-03-29 16:45:01 +0100

    Initial commit

---
+++ www/js/controllers/SurveyCtrl.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ameni on 29/03/2016.
+ */
+angular.module('quizmoo')
+
+    .controller('SurveyCtrl', function($scope) {
+
+
+})
---
+++ www/js/services/surveyService.js
@@ -0,0 +1,9 @@
+angular.module('quizmoo')
+
+.factory('surveyService', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+
+
+});
